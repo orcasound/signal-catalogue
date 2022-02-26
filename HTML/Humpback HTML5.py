6fc4101bda8ca6a639236b95e3c474d40724f068
Emily Vierling <emvierling@gmail.com>
2022-02-25 22:27:04 -0800

    Add files via upload
    Adapted from the column format on WordPress in order to align spectrogram and audio vertically

---
+++ HTML/Humpback HTML5.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+<h3>Whup</h3>
+<div class="wp-block-columns are-vertically-aligned-center">
+<div class="wp-block-cloumn is-vertically-aligned-center" style="flex-basis:66.66%">
+<figure class="wp-block-image size-full"><img src="https://www.orcasound.net/wp2017/wp-content/uploads/2022/02/Linear-Frequency-Scaling-Whup.png"></figure>
+</div>
+
+
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis=33.33%">
+<figure class="wp-block-audio aligncenter"><audio controls src="https://www.orcasound.net/wp2017/wp-content/uploads/2022/02/Whup.mp3"><audio controls src="https://www.orcasound.net/wp2017/wp-content/uploads/2022/02/Whup.ogg"></audio></figure>
+</div>
+</div>
+
+<h3>Grunt</h3>
+<div class="wp-block-columns are-vertically-aligned-center">
+<div class="wp-block-cloumn is-vertically-aligned-center" style="flex-basis:66.66%">
+<figure class="wp-block-image size-full"><img src="https://www.orcasound.net/wp2017/wp-content/uploads/2022/02/Linear-Frequency-Scaling-Grunt.png"></figure>
+</div>
+
+
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis=33.33%">
+<figure class="wp-block-audio aligncenter"><audio controls src="https://www.orcasound.net/wp2017/wp-content/uploads/2022/02/Grunt.mp3"><audio controls src="https://www.orcasound.net/wp2017/wp-content/uploads/2022/02/Grunt.ogg"></audio></figure>
+</div>
+</div>
+
+<h3>Ascending Moan</h3>
+<div class="wp-block-columns are-vertically-aligned-center">
+<div class="wp-block-cloumn is-vertically-aligned-center" style="flex-basis:66.66%">
+<figure class="wp-block-image size-full"><img src="https://www.orcasound.net/wp2017/wp-content/uploads/2022/02/Linear-Frequency-Scaling-Ascending-Moan.png"></figure>
+</div>
+
+
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis=33.33%">
+<figure class="wp-block-audio aligncenter"><audio controls src="https://www.orcasound.net/wp2017/wp-content/uploads/2022/02/Ascending-Moan.mp3"><audio controls src="https://www.orcasound.net/wp2017/wp-content/uploads/2022/02/Ascending-Moan.ogg"></audio></figure>
+</div>
+</div>
+
+<h3>Descending Moan</h3>
+<div class="wp-block-columns are-vertically-aligned-center">
+<div class="wp-block-cloumn is-vertically-aligned-center" style="flex-basis:66.66%">
+<figure class="wp-block-image size-full"><img src="https://www.orcasound.net/wp2017/wp-content/uploads/2022/02/Linear-Frequency-Scaling-Descending-Moan.png"></figure>
+</div>
+
+
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis=33.33%">
+<figure class="wp-block-audio aligncenter"><audio controls src="https://www.orcasound.net/wp2017/wp-content/uploads/2022/02/Descending-Moan.mp3"><audio controls src="https://www.orcasound.net/wp2017/wp-content/uploads/2022/02/Descending-Moan.ogg"></audio></figure>
+</div>
+</div>
+
+<h3>Moan</h3>
+<div class="wp-block-columns are-vertically-aligned-center">
+<div class="wp-block-cloumn is-vertically-aligned-center" style="flex-basis:66.66%">
+<figure class="wp-block-image size-full"><img src="https://www.orcasound.net/wp2017/wp-content/uploads/2022/02/Linear-Frequency-Scaling-Moan.png"></figure>
+</div>
+
+
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis=33.33%">
+<figure class="wp-block-audio aligncenter"><audio controls src="https://www.orcasound.net/wp2017/wp-content/uploads/2022/02/Moan.mp3"><audio controls src="https://www.orcasound.net/wp2017/wp-content/uploads/2022/02/Moan.ogg"></audio></figure>
+</div>
+</div>
+
+
+<h3>Upsweep</h3>
+<div class="wp-block-columns are-vertically-aligned-center">
+<div class="wp-block-cloumn is-vertically-aligned-center" style="flex-basis:66.66%">
+<figure class="wp-block-image size-full"><img src="https://www.orcasound.net/wp2017/wp-content/uploads/2022/02/Linear-Frequency-Scaling-Upsweep.png"></figure>
+</div>
+
+
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis=33.33%">
+<figure class="wp-block-audio aligncenter"><audio controls src="https://www.orcasound.net/wp2017/wp-content/uploads/2022/02/Upsweep.mp3"><audio controls src="https://www.orcasound.net/wp2017/wp-content/uploads/2022/02/Upsweep.ogg"></audio></figure>
+</div>
+</div>
+
+<h3>Trumpet</h3>
+<div class="wp-block-columns are-vertically-aligned-center">
+<div class="wp-block-cloumn is-vertically-aligned-center" style="flex-basis:66.66%">
+<figure class="wp-block-image size-full"><img src="https://www.orcasound.net/wp2017/wp-content/uploads/2022/02/Linear-Frequency-Scaling-Trumpet.png"></figure>
+</div>
+
+
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis=33.33%">
+<figure class="wp-block-audio aligncenter"><audio controls src="https://www.orcasound.net/wp2017/wp-content/uploads/2022/02/Trumpet.mp3"><audio controls src="https://www.orcasound.net/wp2017/wp-content/uploads/2022/02/Trumpet.ogg"></audio></figure>
+</div>
+</div>
+
+<h3>Growl</h3>
+<div class="wp-block-columns are-vertically-aligned-center">
+<div class="wp-block-cloumn is-vertically-aligned-center" style="flex-basis:66.66%">
+<figure class="wp-block-image size-full"><img src="https://www.orcasound.net/wp2017/wp-content/uploads/2022/02/Linear-Frequency-Scaling-Growl.png"></figure>
+</div>
+
+
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis=33.33%">
+<figure class="wp-block-audio aligncenter"><audio controls src="https://www.orcasound.net/wp2017/wp-content/uploads/2022/02/Growl.mp3"><audio controls src="https://www.orcasound.net/wp2017/wp-content/uploads/2022/02/Growl.ogg"></audio></figure>
+</div>
+</div>
+
+<h3>Creak</h3>
+<div class="wp-block-columns are-vertically-aligned-center">
+<div class="wp-block-cloumn is-vertically-aligned-center" style="flex-basis:66.66%">
+<figure class="wp-block-image size-full"><img src="https://www.orcasound.net/wp2017/wp-content/uploads/2022/02/Linear-Frequency-Scaling-Creak.png"></figure>
+</div>
+
+
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis=33.33%">
+<figure class="wp-block-audio aligncenter"><audio controls src="https://www.orcasound.net/wp2017/wp-content/uploads/2022/02/Creak.mp3"><audio controls src="https://www.orcasound.net/wp2017/wp-content/uploads/2022/02/Creak.ogg"></audio></figure>
+</div>
+</div>
+
+<h3>Buzz</h3>
+<div class="wp-block-columns are-vertically-aligned-center">
+<div class="wp-block-cloumn is-vertically-aligned-center" style="flex-basis:66.66%">
+<figure class="wp-block-image size-full"><img src="https://www.orcasound.net/wp2017/wp-content/uploads/2022/02/Linear-Frequency-Scaling-Buzz.png"></figure>
+</div>
+
+
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis=33.33%">
+<figure class="wp-block-audio aligncenter"><audio controls src="https://www.orcasound.net/wp2017/wp-content/uploads/2022/02/Buzz.mp3"><audio controls src="https://www.orcasound.net/wp2017/wp-content/uploads/2022/02/Buzz.ogg"></audio></figure>
+</div>
+</div>
+
+<h3>Shriek</h3>
+<div class="wp-block-columns are-vertically-aligned-center">
+<div class="wp-block-cloumn is-vertically-aligned-center" style="flex-basis:66.66%">
+<figure class="wp-block-image size-full"><img src="https://www.orcasound.net/wp2017/wp-content/uploads/2022/02/Linear-Frequency-Scaling-Shriek.png"></figure>
+</div>
+
+
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis=33.33%">
+<figure class="wp-block-audio aligncenter"><audio controls src="https://www.orcasound.net/wp2017/wp-content/uploads/2022/02/Shriek.mp3"><audio controls src="https://www.orcasound.net/wp2017/wp-content/uploads/2022/02/Shriek.ogg"></audio></figure>
+</div>
+</div>
+
+<h3>Chirp</h3>
+<div class="wp-block-columns are-vertically-aligned-center">
+<div class="wp-block-cloumn is-vertically-aligned-center" style="flex-basis:66.66%">
+<figure class="wp-block-image size-full"><img src="https://www.orcasound.net/wp2017/wp-content/uploads/2022/02/Linear-Frequency-Scaling-Chirp.png"></figure>
+</div>
+
+
+<div class="wp-block-column is-vertically-aligned-center" style="flex-basis=33.33%">
+<figure class="wp-block-audio aligncenter"><audio controls src="https://www.orcasound.net/wp2017/wp-content/uploads/2022/02/Chirp.mp3"><audio controls src="https://www.orcasound.net/wp2017/wp-content/uploads/2022/02/Chirp.ogg"></audio></figure>
+</div>
+</div>
+
